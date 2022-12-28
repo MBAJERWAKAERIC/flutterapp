5d39d8fa8803cc58d5595d2ca9019f67d784ed41
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2022-12-28 20:01:12 +0300

    widgets

---
+++ learningdart/lib/screens/home.dart
@@ -0,0 +1,4 @@
+import 'package:bmi/constants/app_constants.dart';
+import 'package:bmi/widgets/left_bar.dart';
+import 'package:bmi/widgets/right_bar.dart';
+import 'package:flutter/material.dart';
