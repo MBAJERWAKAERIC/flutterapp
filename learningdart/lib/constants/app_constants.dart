7ddc5d220d4667b04d82d2f2cd49f8aba1724b36
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2022-12-28 19:45:11 +0300

    right and left

---
+++ learningdart/lib/constants/app_constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+Import 'package:hexcolor/hexcolor.dart';
+
+final color mainHexColor = HexColor("#1c1c1c");
+final color mainHexColor = HexColor("#FCC91C");
---
+++ learningdart/lib/widgets/left_bar.dart
@@ -0,0 +1,28 @@
+import 'dart:html';
+
+import 'package:bmi/constants/app_constants.dart';
+import 'package:flutter/material.dart';
+
+class LeftBar extends StatelessWigte {
+  final double BarWidth;
+  const leftBar({Key key, @required this.barWidth}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Row( 
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Container(
+          height: 25,
+          width: barWidth,
+          decoration: BoxDecoration(
+            borderadious:borderRadius.Only(
+              topLeft:Radious.circular(20),
+              bottomLeft:Radious.circular(20),
+            )
+          coolor: accentHexColor),
+        )
+      ],
+      );
+  }
+}
